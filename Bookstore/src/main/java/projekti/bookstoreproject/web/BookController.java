d8a06082c2c50e60f7e22c401d101dbf96289a4c
Heidi <heidiahlgren@hotmail.com>
2024-09-09 13:22:00 +0300

    C3 4. Edit Bookstore DONE

--- Bookstore/src/main/java/projekti/bookstoreproject/web/BookController.java
+++ Bookstore/src/main/java/projekti/bookstoreproject/web/BookController.java
@@ -1,13 +1,17 @@
 package projekti.bookstoreproject.web;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.ui.Model;
 
 import projekti.bookstoreproject.domain.Book;
 import projekti.bookstoreproject.domain.BookRepository;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 
@@ -25,13 +29,26 @@ private BookRepository repository;
     @RequestMapping(value="/add")
     public String addBook(Model model){
         model.addAttribute("book", new Book());
-        return "addstudent";
+        return "addbook";
     }
     
     @PostMapping("/save")
     public String save(Book book) {
         repository.save(book);
         return "redirect:booklist";
+    }
+
+    @RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+    public String deleteBook(@PathVariable("id")Long bookId, Model model){
+        repository.deleteById(bookId);
+        return "redirect:../booklist";
+    }
+
+    @RequestMapping(value = "/edit/{id}", method = RequestMethod.GET)
+    public String editBook(@PathVariable("id") Long bookId, Model model){
+        Optional<Book> optionalBook = repository.findById(bookId);
+        model.addAttribute("book", optionalBook.get());
+        return "editbook";
     }
     
 }
