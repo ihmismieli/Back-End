667f4451b0c5fa0c662812a7be736cc35e90bd08
Heidi <heidiahlgren@hotmail.com>
2024-09-16 16:02:26 +0300

    C4 1/2 OneToMany DONE

--- Bookstore/src/main/java/projekti/bookstoreproject/web/BookController.java
+++ Bookstore/src/main/java/projekti/bookstoreproject/web/BookController.java
@@ -13,31 +13,30 @@ import projekti.bookstoreproject.domain.CategoryRepository;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
-
 @Controller
 public class BookController {
 
-@Autowired
-private BookRepository repository;
+    @Autowired
+    private BookRepository repository;
 
-@Autowired
-private CategoryRepository cRepository;
+    @Autowired
+    private CategoryRepository cRepository;
 
-    //show all books
-    @RequestMapping(value={"/", "/booklist"})
-    public String bookList (Model model){
+    // show all books
+    @RequestMapping(value = { "/", "/booklist" })
+    public String bookList(Model model) {
         model.addAttribute("books", repository.findAll());
         return "booklist";
     }
 
-    @RequestMapping(value="/add")
-    public String addBook(Model model){
+    @RequestMapping(value = "/add")
+    public String addBook(Model model) {
         model.addAttribute("book", new Book());
-        //hakee kaikki tietokannassa olevat categoryt
+        // hakee kaikki tietokannassa olevat categoryt
         model.addAttribute("categories", cRepository.findAll());
         return "addbook";
     }
-    
+
     @PostMapping("/save")
     public String save(Book book) {
         repository.save(book);
@@ -45,16 +44,16 @@ private CategoryRepository cRepository;
     }
 
     @RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
-    public String deleteBook(@PathVariable("id")Long bookId, Model model){
+    public String deleteBook(@PathVariable("id") Long bookId, Model model) {
         repository.deleteById(bookId);
         return "redirect:../booklist";
     }
 
     @RequestMapping(value = "/edit/{id}", method = RequestMethod.GET)
-    public String editBook(@PathVariable("id") Long bookId, Model model){
+    public String editBook(@PathVariable("id") Long bookId, Model model) {
         model.addAttribute("book", repository.findById(bookId));
         model.addAttribute("categories", cRepository.findAll());
         return "editbook";
     }
-    
+
 }
